d84f62defb634a39043b5c3f5e9eff5479dee51e
Darran Holmes <darranholmes74@gmail.com>
2023-03-31 16:45:32 -0700

    finished challenge 10

---
+++ python/data_structures/linked_list.py
@@ -0,0 +1,93 @@
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def __str__(self):
+        if self.head is None:
+            return f'{None}'
+
+        current = self.head
+        llstr = ''
+
+        while current:
+            llstr += '{ ' + str(current.value) + ' }' + ' -> '
+            current = current.next
+
+        if current is None:
+            llstr += '' + str(None)
+
+        return llstr
+
+    def insert(self, value):
+        node = Node(value, self.head)
+        if self.head is not None:
+            node._next = self.head
+            self.head = node
+        self.head = node
+
+    def includes(self, value):
+        itr = self.head
+        while itr:
+            if itr.value == value:
+                return True
+            else:
+                itr = itr.next
+
+            continue
+
+    def append(self, new_value):
+        # node = Node(new_value)
+        if self.head is None:
+            self.head = Node(new_value)
+            return
+
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(new_value)
+
+    def insert_before(self, value1, value2):
+        if self.head is None:
+            self.head = Node(value2)
+            return
+
+        if self.head.value == value1:
+            self.head = Node(value2, self.head)
+            return
+
+        current = self.head
+        while current.next is not None:
+            if current.next.value == value1:
+                current.next = Node(value2, current.next)
+                return
+            current = current.next
+
+
+
+
+
+
+class Node:
+    def __init__(self, value, _next=None):
+        self.value = value
+        self.next = _next
+
+
+class TargetError:
+    pass
+
+
+if __name__ == '__main__':
+    test = LinkedList()
+    test.insert("apple")
+    test.insert("banana")
+    test.append("cucumber")
+    test.insert_before("apple", "cucumber")
+    print(test)
+    test.__str__()
+
+
+
+
+
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,40 @@
+from data_structures.linked_list import Node
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Stack:
     """
     Put docstring here
     """
 
-    def __init__(self):
+    def __init__(self, top=None):
         # initialization here
-        pass
+        self.top = top
 
-    def some_method(self):
+    def push(self, value):
         # method body here
-        pass
+        node = Node(value)
+        node._next = self.top
+        self.top = node
+
+
+
+    def pop(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            value = self.top.value
+            self.top = self.top._next
+            return value
+
+    def peek(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        return self.top.value
+
+    def is_empty(self):
+        try:
+            self.peek()
+            return False
+        except InvalidOperationError:
+            return True
+
