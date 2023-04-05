1091e531c0289e67b3519faf872dda2f4f3cb316
Darran Holmes <darranholmes74@gmail.com>
2023-04-05 15:10:48 -0700

    finished code challenge 13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,21 @@
 from data_structures.queue import Queue
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(list):
+    stack = []
+    open_brackets = "[({"
+    close_brackets = "})]"
+    for char in list:
+        if char in open_brackets:
+            stack.append(char)
+        elif char in close_brackets:
+            if not stack:
+                return False
+            top = stack.pop()
+            if open_brackets.index(top) != close_brackets.index(char):
+                return False
+
+        return True
+
+
+multi_bracket_validation("[]")
