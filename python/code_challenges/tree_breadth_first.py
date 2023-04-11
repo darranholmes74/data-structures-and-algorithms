b1e4f6e292c35fef6c71a6f125aceac355418dba
Darran Holmes <darranholmes74@gmail.com>
2023-04-11 14:31:20 -0700

    first breadth first commit

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,20 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    def walk(root, front, rear):
+        if root is None:
+            return None
+        if root.value is not None:
+            root.value == front
+            walk(root.left)
+            walk(root.right)
+        if root.left is not None:
+            front == root.left
+            tree.append(front)
+        if root.right is not None:
+            rear == root.left
+            tree.append(rear)
+
+        return walk(root, front, rear)
+
