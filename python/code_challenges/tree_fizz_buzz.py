918164ab5b996afe67db5370bc902189ae836908
Darran Holmes <darranholmes74@gmail.com>
2023-04-12 16:02:39 -0700

    turning challenge 18

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,24 @@
-from data_structures.binary_tree import BinaryTree
+import copy
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+    if not tree:
+        return []
+
+    copy_kary = copy.deepcopy(tree)
+
+    def walk(node):
+        if node.value % 5 == 0 and node.value % 3 == 0:
+            node.value = "FizzBuzz"
+        elif node.value % 3 == 0:
+            node.value = "Fizz"
+        elif node.value % 5 == 0:
+            node.value = "Buzz"
+        else:
+            node.value = str(node.value)
+        for child in node.children:
+            walk(child)
+
+    walk(copy_kary.root)
+    return copy_kary
+
