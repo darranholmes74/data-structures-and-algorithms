efebb66cb10e0b9c4805f0163717ae13960be762
Darran Holmes <darranholmes74@gmail.com>
2023-04-28 16:32:10 -0700

    first turn in for code challenge 30

---
+++ python/data_structures/hashtable.md
@@ -0,0 +1,10 @@
+# Challenge Title
+Hash Tables
+
+
+## Approach & Efficiency
+I am still struggling to wrap my head around the set function. The other functions I feel like are correctly implemented.
+I need some more time with this code over the weekend to get the last test to pass.
+
+## Solution
+Have 2 out of the 3 tests passing for now.
--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,50 @@
 class Hashtable:
-    """
-    Put docstring here
-    """
+    def __init__(self, size=1024):
+        self._size = size
+        self._buckets = size * [None]
 
-    def __init__(self):
-        # initialization here
-        pass
+    def set(self, key, value):
+        # TODO: Hash the key
+        h = self.hash(key)
 
-    def some_method(self):
-        # method body here
-        pass
+        found = False
+        for k, v in enumerate(self._buckets[h] or []):
+            if v[0] == key:
+                self._buckets[h][k] = (key, value)
+                found = True
+                break
+        if not found:
+            self._buckets[h] = self._buckets[h] or []
+            self._buckets[h].append((key, value))
+
+    def get(self, key):
+        # TODO: Hash the key
+        h = self.hash(key)
+        for k, v in self._buckets[h] or []:
+            if k == key:
+                return v
+        return None
+
+    def has(self, key):
+        # TODO: Hash the key
+        h = self.hash(key)
+        for k, v in self._buckets[h] or []:
+            if k == key:
+                return True
+        return False
+
+    def keys(self):
+        keys = []
+        for bucket in self._buckets:
+            if bucket:
+                for k, v in bucket:
+                    keys.append(k)
+        return keys
+
+    def hash(self, key):
+        hash_total = 0
+        for char in key:
+            hash_total += ord(char)
+        primed_number = hash_total * len(key) * 19
+        index = primed_number % self._size
+        return index
