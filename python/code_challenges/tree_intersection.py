afa59a071fb5a21e6e4dc4ddcf744f3cd5d9d0ef
Darran Holmes <darranholmes74@gmail.com>
2023-05-02 18:06:51 -0700

    first turn in for challenge 32

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,28 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree_a, tree_b):
+
+    ht = Hashtable()
+
+    def add_to_ht(node):
+        if node:
+            ht.set(node.value, None)
+            add_to_ht(node.left)
+            add_to_ht(node.right)
+
+    def check(node, result):
+        if node:
+            if ht.has(node.value):
+                result.append(node.value)
+            check(node.left, result)
+            check(node.right, result)
+
+    add_to_ht(tree_a.root)
+
+    result = []
+    check(tree_b.root, result)
+
+    return result
+
